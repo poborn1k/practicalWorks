ed68a1dea110af45cb99ac7c0779bbfb95679eec
poborn1k <142586491+poborn1k@users.noreply.github.com>
2023-09-18 09:40:22 +0300

    Add files via upload

---
+++ src/Ball.java
@@ -0,0 +1,39 @@
+package pw2;
+
+public class Ball {
+    private double x = 0.0;
+    private double y = 0.0;
+    //private double speed = 0.0;
+
+    public Ball() {}
+    public Ball(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setXY(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public void move( double newX, double newY){
+        x += newX;
+        y += newY;
+    }
+//    public void  setXYSpeed(double speed) {
+//        this.speed = speed;
+//    }
+    public String toString() {
+        return "Ball @ (" + x + ", " + y + ")";
+    }
+}
---
+++ src/TestAuthor.java
@@ -0,0 +1,13 @@
+package pw2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author1 = new Author("Bob", "someemail1@gmail.com", 'M');
+        Author author2 = new Author("Karen", "someemail2@gmail.com", 'F');
+
+        System.out.println(author1.toString() + "\n" + author2.toString());
+
+        author2.setEmail("newemail@yandex.ru");
+        System.out.println(author2.toString());
+    }
+}
