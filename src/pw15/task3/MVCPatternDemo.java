74014b8d2498f9230ee4812dda41f6acae1155a6
poborn1k <klanke@yandex.ru>
2023-10-03 08:53:15 +0300

    afs10

--- src/pw15/task3/MVCPatternDemo.java
+++ src/pw15/task3/MVCPatternDemo.java
@@ -1,15 +1,49 @@
 package pw15.task3;
 
+import java.util.Scanner;
+
 public class MVCPatternDemo {
     public static void main(String[] args) {
         Client model = retrieveClientFromDatabase();
         ClientView view = new ClientView();
         ClientController controller = new ClientController(model, view);
-        controller.updateView();
-        controller.setClientPayment(2499);
-        controller.setClientPaid(false);
-        System.out.println("After updating, Employee Information are as follows");
-        controller.updateView();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("""
+                    Menu
+                    1 - Show client information
+                    2 - Change client payment
+                    3 - Change payment status
+                    4 - Get client name
+                    5 - Get client payment
+                    6 - Get client date of join
+                    7 - Get client payment status
+                    0 - Exit""");
+
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 -> controller.updateView();
+                case 2 -> {
+                    System.out.print("Enter new payment: ");
+                    controller.setClientPayment(scan.nextInt());
+                }
+                case 3 -> {
+                    System.out.print("Enter new status of payment (true/false): ");
+                    controller.setClientPaid(scan.nextBoolean());
+                }
+                case 4 -> System.out.println("Client name: " + controller.getClientName());
+                case 5 -> System.out.println("Client payment: " + controller.getClientPayment());
+                case 6 -> System.out.println("Client date of join: " + controller.getClientDateOfJoin());
+                case 7 -> System.out.println("Client payment status: " + controller.clientsPaid());
+                case 0 -> {
+                    System.out.println("Program is closed");
+                    return;
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static Client retrieveClientFromDatabase() {
